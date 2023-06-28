0b2dbea13f31600c3b19e40b4be36a87edf6fa9d
dblock8 <jdavenport683@gmail.com>
2023-06-28 02:41:58 +0000

    new file:   best 	new file:   deleteme.py 	new file:   helloworld.py 	new file:   no_shebang.py 	new file:   practice 	new file:   py3_shebang.py

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+#this is a comment. Comments begin with a #
+#Default usage of a print () is as follows:
+#print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+#
+print("Hello,")     # prints 'Hello,' to the screen with implied \n
+print(" world!")    #prints ' world!' to the screen with implied \n
+
+
+#argument- the value to be passed to the function (world)
+
+#function -A call to pre-written code. Python comes with many built-in functions, or you can write your own
