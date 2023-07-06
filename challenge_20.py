944d5004df9217d0a655cf86c26c168b025f0ce0
dblock8 <jdavenport683@gmail.com>
2023-07-06 16:14:20 +0000

    About complex lists

--- challenge_20.py
+++ challenge_20.py
@@ -1,18 +1,25 @@
 # create wordbank list
-avengers = ['ironman', 'hulk', 'captian america']
+wordbank= ["indentation", "spaces"] 
+
 # create student list
-students = ['jason', 'john', 'sally']
+tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+
 
 # add integer 4 to the wordbank
+wordbank.append(4)
 
 
 # prompt user for number 0-20 be sure convert to int
+num = input("Pick a number")
 
+num = int(input=("Pick a number"))
 
 # Use number from user to slice list
+choice= int(input("Pick a student number!"))
+student_name= tlgstudents[choice]
 
 
 # print output
-
+print(f'{student_name} always uses, {wordbank[1]} {wordbank[2]}, to indent.')
 
 
