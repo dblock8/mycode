3e5bbf1335692f8499ab3aed0ea0fabe5583a741
dblock8 <jdavenport683@gmail.com>
2023-07-06 21:59:30 +0000

    Learning about conditional if

---
+++ gitadd.py
@@ -0,0 +1,8 @@
+#!usr/bin/env python3
+
+import git
+
+git add *
+print("What do you want to name your commit?")
+
+    git = input(
---
+++ if.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+print("How old are you?")
+
+age = int(input())
+if age >= 21: 
+        print("Party like a rock star")
+
+else: print("You're not old enough to party")
+
+
