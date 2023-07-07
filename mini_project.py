36587f6869300059814574fd9fc2de771d1e735f
dblock8 <jdavenport683@gmail.com>
2023-07-07 22:24:37 +0000

    mini_if_project

---
+++ mini_project.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+avengers = input("Who is the strongest Avenger?  ironman,  hulk,  captian america,  thor,  spiderman? ")
+ 
+strongest_avenger = (' hulk ')
+
+if avengers == ("hulk"):
+    print(avengers + "smashes all")  
+
+else:
+    print(avengers +  "is too weak," + strongest_avenger + "is the strongest!")
